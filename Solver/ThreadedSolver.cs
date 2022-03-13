13d66107464d90b88cbc30489f757dadc12d85de
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-03-13 01:13:26 -0300

    added threads

--- Solver/ThreadedSolver.cs
+++ Solver/ThreadedSolver.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System;
 using PrimeNumbersThreaded.Extensions;
+using System.Threading;
 
 namespace PrimeNumbersThreaded.PrimesSolver
 {
-    public class ThreadedSolver : PrimesSolver
+    public sealed class ThreadedSolver : PrimesSolver
     {
-        public int ThreadsAmount { get; set; } = 2;
+        public int ThreadsAmount { get; set; };
 
-        private IList<int> GetTheadsIntervals(IList<int> numbers)
+        /// <summary>
+        /// Splits the intervals of numbers that will be solved by each thread, 
+        /// which are separated two by two in the returned list 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private IList<int> GetThreadsIntervals(IList<int> numbers)
         {
             var numbersAmount = numbers.Count;
             var step = (int) Math.Ceiling( (decimal) numbersAmount / ThreadsAmount);
@@ -18,10 +25,23 @@ namespace PrimeNumbersThreaded.PrimesSolver
             return range.ToList();
         }
 
+        /// <inheritdoc/>
         protected override int Solve(IList<int> numbers, out int executionTime)
         {
             var primesAmount = 0;
             executionTime = 0;
+
+            var threadsIntervals = GetThreadsIntervals(numbers);
+
+            for(var i = 0; i < ThreadsAmount; i++)
+            {
+                var intervalBegin = threadsIntervals.ElementAt(i);
+                var intervalEnd = threadsIntervals.ElementAt(i + 1);
+                var intervalNumbers = numbers.ToList().GetRange(intervalBegin, intervalEnd);
+                
+                var thread = new Thread(() => primesAmount += FindPrimesAmount(intervalNumbers));
+                thread.Start();
+            }
 
             return 0;
         }
